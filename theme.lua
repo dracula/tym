691bc66ac76daf76fbda56514bdc3c326297e094
Gustavo Vínicios de Souza <gnsfujiwara@arch.localdomain>
2021-09-09 16:15:49 -0300

    Updated

--- theme.lua
+++ theme.lua
@@ -1,21 +1,23 @@
+-- Dracula
 local bg = '#282a36'
 local fg = '#f8f8f2'
+
 return {
-  color_background = bg,
-  color_foreground = fg,
-  color_bold = fg,
-  color_cursor = fg,
-  color_cursor_foreground = bg,
-  color_highlight = fg,
+  color_background           = bg,
+  color_foreground           = fg,
+  color_bold                 = fg,
+  color_cursor               = fg,
+  color_cursor_foreground    = bg,
+  color_highlight            = fg,
   color_highlight_foreground = bg,
-  color_0 = bg,
+  color_0 = '#282a36',
   color_1 = '#ff5555',
   color_2 = '#50fa7b',
   color_3 = '#f1fa8c',
   color_4 = '#bd93f9',
   color_5 = '#ff79c6',
   color_6 = '#8be9fd',
-  color_7 = fg,
+  color_7 = '#f8f8f2',
   color_8 = '#4d4d4d',
   color_9 = '#ff6e67',
   color_10 = '#5af78e',
