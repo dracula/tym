eea1de696c70509a39b0bada945b455d74ce08a5
Gustavo Vínicios de Souza <gnsfujiwara@arch.localdomain>
2021-09-09 05:42:32 -0300

    Dracula Theme

---
+++ theme.lua
@@ -0,0 +1,28 @@
+-- Configurando o tema Dracula
+local bg = '#282a36'
+local fg = '#f8f8f2'
+return {
+  color_background = bg,
+  color_foreground = fg,
+  color_bold = fg,
+  color_cursor = fg,
+  color_cursor_foreground = bg,
+  color_highlight = fg,
+  color_highlight_foreground = bg,
+  color_0 = bg,
+  color_1 = '#ff5555',
+  color_2 = '#50fa7b',
+  color_3 = '#f1fa8c',
+  color_4 = '#bd93f9',
+  color_5 = '#ff79c6',
+  color_6 = '#8be9fd',
+  color_7 = fg,
+  color_8 = '#4d4d4d',
+  color_9 = '#ff6e67',
+  color_10 = '#5af78e',
+  color_11 = '#f4f99d',
+  color_12 = '#caa9fa',
+  color_13 = '#ff92d0',
+  color_14 = '#9aedfe',
+  color_15 = '#e6e6e6',
+}
