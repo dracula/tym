a15cf3c8cb90f54ecf4f7855a883a2bbea9463c7
Gustavo Vínicios de Souza <gustavo.vinicios2004@protonmail.com>
2021-10-06 18:41:27 -0300

    Updated

--- theme.lua
+++ theme.lua
@@ -1,29 +1,46 @@
--- Dracula
-local bg = '#282a36'
-local fg = '#f8f8f2'
+		---------------------------
+		--  Dracula Colorscheme  --
+		---------------------------
 
-return {
-  color_background           = bg,
-  color_foreground           = fg,
-  color_bold                 = fg,
-  color_cursor               = fg,
-  color_cursor_foreground    = bg,
-  color_highlight            = fg,
-  color_highlight_foreground = bg,
-  color_0 = '#282a36',
-  color_1 = '#ff5555',
-  color_2 = '#50fa7b',
-  color_3 = '#f1fa8c',
-  color_4 = '#bd93f9',
-  color_5 = '#ff79c6',
-  color_6 = '#8be9fd',
-  color_7 = '#f8f8f2',
-  color_8 = '#4d4d4d',
-  color_9 = '#ff6e67',
-  color_10 = '#5af78e',
-  color_11 = '#f4f99d',
-  color_12 = '#caa9fa',
-  color_13 = '#ff92d0',
-  color_14 = '#9aedfe',
-  color_15 = '#e6e6e6',
+return {	
+	-- Special
+	color_foreground		= '#f8f8f2',
+	color_background		= '#282a36',
+	color_cursor			= '#f8f8f2',
+	color_cursor_foreground		= '#282a36',
+	color_highlight			= '#44475a',
+	color_highlight_foreground	= '#f8f8f2',
+	color_bold			= '#f8f8f2',
+
+	-- Black
+	color_0				= '#21222c',
+	color_8				= '#6272a4',
+
+	-- Red
+	color_1				= '#ff5555',
+	color_9				= '#ff6e6e',
+
+	-- Green
+	color_2				= '#50fa7b',
+	color_10			= '#69ff94',
+
+	-- Yellow
+	color_3				= '#f1fa8c',
+	color_11			= '#ffffa5',
+
+	-- Blue
+	color_4				= '#bd93f9',
+	color_12			= '#d6acff',
+
+	-- Magenta
+	color_5				= '#ff79c6',
+	color_13			= '#ff92df',
+
+	-- Cyan
+	color_6				= '#8be9fd',
+	color_14			= '#a4ffff',
+
+	-- White
+	color_7				= '#f8f8f2',
+	color_15			= '#ffffff',
 }
