5a7cb1dfdc0c2d497ab4de54ebf4b6270ded8ed4
Gustavo Vinícios de Souza <gustavo.vinicios2004@protonmail.com>
2022-05-12 18:13:42 -0300

    Using 2 spaces instead tabs

--- theme.lua
+++ theme.lua
@@ -1,46 +1,44 @@
----------------------------
---  Dracula Colorscheme  --
----------------------------
+-- Dracula Colorscheme
 
-return {	
-	-- Special
-	color_foreground		= '#f8f8f2',
-	color_background		= '#282a36',
-	color_cursor			= '#f8f8f2',
-	color_cursor_foreground		= '#282a36',
-	color_highlight			= '#44475a',
-	color_highlight_foreground	= '#f8f8f2',
-	color_bold			= '#f8f8f2',
+return {
+  -- Special
+  color_foreground = '#f8f8f2',
+  color_background = '#282a36',
+  color_cursor = '#f8f8f2',
+  color_cursor_foreground = '#282a36',
+  color_highlight = '#44475a',
+  color_highlight_foreground = '#f8f8f2',
+  color_bold = '#f8f8f2',
 
-	-- Black
-	color_0				= '#21222c',
-	color_8				= '#6272a4',
+  -- Black
+  color_0 = '#21222c',
+  color_8 = '#6272a4',
 
-	-- Red
-	color_1				= '#ff5555',
-	color_9				= '#ff6e6e',
+  -- Red
+  color_1 = '#ff5555',
+  color_9 = '#ff6e6e',
 
-	-- Green
-	color_2				= '#50fa7b',
-	color_10			= '#69ff94',
+  -- Green
+  color_2 = '#50fa7b',
+  color_10 = '#69ff94',
 
-	-- Yellow
-	color_3				= '#f1fa8c',
-	color_11			= '#ffffa5',
+  -- Yellow
+  color_3 = '#f1fa8c',
+  color_11 = '#ffffa5',
 
-	-- Blue
-	color_4				= '#bd93f9',
-	color_12			= '#d6acff',
+  -- Blue
+  color_4 = '#bd93f9',
+  color_12 = '#d6acff',
 
-	-- Magenta
-	color_5				= '#ff79c6',
-	color_13			= '#ff92df',
+  -- Magenta
+  color_5 = '#ff79c6',
+  color_13 = '#ff92df',
 
-	-- Cyan
-	color_6				= '#8be9fd',
-	color_14			= '#a4ffff',
+  -- Cyan
+  color_6 = '#8be9fd',
+  color_14 = '#a4ffff',
 
-	-- White
-	color_7				= '#f8f8f2',
-	color_15			= '#ffffff',
+  -- White
+  color_7 = '#f8f8f2',
+  color_15 = '#ffffff',
 }
